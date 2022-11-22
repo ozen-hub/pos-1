5f95fd88db8b4a408818dce51700fc3ab4d726e4
ozen-hub <info.ozenhub@gmail.com>
2022-11-22 22:32:13 +0530

    10

--- src/main/java/com/bootcamp/pos/repo/CustomerRepo.java
+++ src/main/java/com/bootcamp/pos/repo/CustomerRepo.java
@@ -1,9 +1,11 @@
 package com.bootcamp.pos.repo;
 
 import com.bootcamp.pos.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface CustomerRepo extends CrudRepository<Customer,String> {
+@EnableJpaRepositories
+public interface CustomerRepo extends JpaRepository<Customer,String> {
 }
