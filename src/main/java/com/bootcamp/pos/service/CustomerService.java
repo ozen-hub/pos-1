cc609cdebe58633cc5dabea3bfe8474c17820451
ozen-hub <info.ozenhub@gmail.com>
2022-11-22 22:01:57 +0530

    8

--- src/main/java/com/bootcamp/pos/service/CustomerService.java
+++ src/main/java/com/bootcamp/pos/service/CustomerService.java
@@ -17,11 +17,12 @@ public class CustomerService {
     @Autowired
     private CustomerRepo customerRepo;
 
-    public String save(Customer c){
+    public String save(Customer c) {
         customerRepo.save(c);
-        return "Saved "+c.getId();
+        return "Saved " + c.getId();
     }
-    public Customer get(String id){
+
+    public Customer get(String id) {
        /* Optional<Customer> record = customerRepo.findById(id);
         if (record.isPresent()){
             return record.get();
@@ -29,18 +30,25 @@ public class CustomerService {
         return null;*/
         return customerRepo.findById(id).orElse(null);
     }
-    public List<Customer> getAll(){
+
+    public List<Customer> getAll() {
         return (List<Customer>) customerRepo.findAll();
     }
-    public String update(Customer c){
+
+    public String update(Customer c) {
         Customer c1 = customerRepo.findById(c.getId()).orElse(null);
-        if(c1!=null){
+        if (c1 != null) {
             c1.setName(c.getName());
             c1.setSalary(c.getSalary());
             c1.setAddress(c.getAddress());
-            return "Updated "+customerRepo.save(c1).getId();
+            return "Updated " + customerRepo.save(c1).getId();
         }
         return "404 Not Found";
+    }
+
+    public String delete(String id) {
+        customerRepo.deleteById(id);
+        return "Deleted " + id;
     }
 
 }
