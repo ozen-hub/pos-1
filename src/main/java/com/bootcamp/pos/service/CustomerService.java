ac277fce5f60c6207575fe40916e40ec9c5ecf57
ozen-hub <info.ozenhub@gmail.com>
2022-11-22 21:46:23 +0530

    6

--- src/main/java/com/bootcamp/pos/service/CustomerService.java
+++ src/main/java/com/bootcamp/pos/service/CustomerService.java
@@ -1,9 +1,31 @@
 package com.bootcamp.pos.service;
 
+import com.bootcamp.pos.entity.Customer;
+import com.bootcamp.pos.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    public String save(Customer c){
+        customerRepo.save(c);
+        return "Saved "+c.getId();
+    }
+    public Customer get(String id){
+       /* Optional<Customer> record = customerRepo.findById(id);
+        if (record.isPresent()){
+            return record.get();
+        }
+        return null;*/
+        return customerRepo.findById(id).orElse(null);
+    }
+
 }
